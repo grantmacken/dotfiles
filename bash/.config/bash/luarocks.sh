ea6a21d41ab5511ead1d03d9ee80e42f43d00c5a
grantmacken <grantmacken@gmail.com>
2017-10-06 11:18:26 +1300

    paths for luarocks

---
+++ bash/.config/bash/luarocks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LUA_PATH='/home/gmack/.luarocks/share/lua/5.1/?.lua;/home/gmack/.luarocks/share/lua/5.1/?/init.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;./?.lua;/usr/local/share/luajit-2.1.0-beta3/?.lua'
+export LUA_CPATH='/home/gmack/.luarocks/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/?.so;./?.so;/usr/local/lib/lua/5.1/loadall.so'
