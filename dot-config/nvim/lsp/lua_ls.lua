fe080609802aad2edea15da7757611bda5e81aa8
grantmacken <grantmacken@gmail.com>
2025-09-04 08:27:57 +1200

    conf: lua_ls

--- dot-config/nvim/lsp/lua_ls.lua
+++ dot-config/nvim/lsp/lua_ls.lua
@@ -1,7 +1,43 @@
+
+local tbl_cmd = { 'host-spawn', 'lua-language-server' }
+local tbl_settings = {
+  Lua = {
+    runtime = {
+      version = 'LuaJIT',
+      path = vim.split(package.path, ';'),
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global, etc.
+      globals = { 'vim' },
+      disable = { 'duplicate-set-field', 'need-check-nil' },
+      -- Don't make workspace diagnostic, as it consumes too much CPU and RAM
+      workspaceDelay = -1,
+    },
+    -- Make the server aware of Neovim runtime files
+    workspace = {
+      checkThirdParty = false,
+      library = { vim.env.VIMRUNTIME },
+      ignoreSubmodules = true,
+      -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+      -- library = vim.api.nvim_get_runtime_file("", true)
+    },
+    telemetry = {
+      enable = false,
+    },
+    hint = { -- inlay hints
+      enable = true,
+    },
+    codeLens = {
+      enable = true,
+    },
+  }
+}
+
 return {
-  cmd = { 'host-spawn', 'lua-language-server'},
+  cmd = tbl_cmd,
   filetypes = { 'lua' },
   root_markers = {
     '.git',
   },
+  settings = tbl_settings,
 }
