af5768a75a702cf5363e0a8dfaaf1117039f1e76
grantmacken <grantmacken@gmail.com>
2025-09-11 16:55:22 +1200

    reset

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -18,6 +18,23 @@ end
 vim.g.mapleader = vim.keycode("<space>") -- Set leader key to space
 
 local keymap = require('util').keymap
- keymap('<Leader>t', ':botright sp | term<CR>', 'Open [t]erminal')
+keymap('<Leader>t', ':botright sp | term<CR>', 'Open [t]erminal')
+keymap('<esc>',[[<C-\><C-n>]], 'Close [t]erminal','t')
+keymap('<Leader>r', ':restart<CR>', '[r]estart nvim')
+keymap('<leader>q', '<cmd>lua vim.diagnostic.setqflist()<CR>', 'set [q]uickfix list')
 
 
+local group = vim.api.nvim_create_augroup('my_group', {})
+local au = function(event, pattern, callback, desc)
+    vim.api.nvim_create_autocmd(event, { group = group, pattern = pattern, callback = callback, desc = desc })
+end
+
+--- Highlight on yank
+au('TextYankPost', '*',function() vim.hl.on_yank() end,'Highlight yanked text')
+
+local start_terminal_insert = vim.schedule_wrap(function(data)
+  -- Try to start terminal mode only if target terminal is current
+  if not (vim.api.nvim_get_current_buf() == data.buf and vim.bo.buftype == 'terminal') then return end
+  vim.cmd('startinsert')
+end)
+au('TermOpen', 'term://*', start_terminal_insert, 'Start builtin terminal in Insert mode')
